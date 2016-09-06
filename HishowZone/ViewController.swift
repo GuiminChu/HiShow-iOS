7c17a8f701d3681d2dd320508006042773024f43
Chu Guimin <455824173@qq.com>
2016-09-06 10:24:53 +0800

    Initial commit

---
+++ HishowZone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HishowZone
+//
+//  Created by Chu Guimin on 16/9/6.
+//  Copyright © 2016年 Chu Guimin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,27 @@
+platform :ios, '9.0'
+
+target 'HishowZone' do
+
+  use_frameworks!
+
+  # Pods for HishowZone
+
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+  pod 'Kingfisher'
+  pod 'AlamofireImage'
+  pod 'SKPhotoBrowser'
+  pod 'MJRefresh'
+  pod 'Navi'
+
+  target 'HishowZoneTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'HishowZoneUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
